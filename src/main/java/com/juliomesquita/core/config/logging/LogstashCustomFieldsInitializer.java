9037750fd9e27f808b924de49b79c6efa2c8b34a
juliocesarmcamilo@gmail.com <juliocesarmcamilo@gmail.com>
2025-03-24 09:49:43 -0300

    Adicionado alguma config para logs

---
+++ src/main/java/com/juliomesquita/core/config/logging/LogstashCustomFieldsInitializer.java
@@ -0,0 +1,25 @@
+package com.juliomesquita.core.config.logging;
+
+import ch.qos.logback.classic.LoggerContext;
+import net.logstash.logback.argument.StructuredArguments;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogstashCustomFieldsInitializer implements ApplicationListener<ApplicationReadyEvent> {
+   @Value("${spring.application.name}")
+   private String appName;
+
+   @Override
+   public void onApplicationEvent(ApplicationReadyEvent event) {
+      LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+      context.getLogger("ROOT")
+              .info(
+                      "Application Started",
+                      StructuredArguments.keyValue("app_name", appName)
+              );
+   }
+}
